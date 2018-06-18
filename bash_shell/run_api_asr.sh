3baad14953600db3209f1dcf223ca3b91e695708
albertzhang24 <skyline24a@gmail.com>
2018-06-18 11:27:27 -0700

    Delete run_api_asr.sh

--- bash_shell/run_api_asr.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/sh
-
-count=0
-while [ $count -le 17 ]; do
-  chmod 755 ./asr.sh
-  ./asr.sh
-
-# Pipe data into the while loop
-done < /Users/albertzhang/desktop/text_files/biggerintervals.txt
